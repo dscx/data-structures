8b65722891e2b41efc623162956f760acd3f8b87
dscx <dnchmbrln@gmail.com>
2014-07-23 15:34:15 -0700

    completed queue and stack

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,17 +3,32 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var instanceSize = 0;
+  var nextUp = 1;
+  var newItem = 1;
 
-  // Implement the methods below
 
   someInstance.enqueue = function(value){
+    instanceSize++;
+    storage[newItem] = value;
+    newItem++;
   };
 
   someInstance.dequeue = function(){
+    var save = storage[nextUp];
+    delete storage[nextUp];
+    nextUp++;
+    if(instanceSize > 0){instanceSize--;}
+    return save;
   };
 
   someInstance.size = function(){
+    return instanceSize;
   };
 
   return someInstance;
 };
+
+
+
+
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,31 @@
 var makeStack = function(){
-  var someInstance = {};
+  
 
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var someInstance = {};
+
+  var instanceSize = 0;
   // Implement the methods below
   someInstance.push = function(value){
+    instanceSize++;
+    storage[instanceSize] = value;
   };
 
   someInstance.pop = function(){
+    var save = storage[instanceSize];
+    delete storage[instanceSize];
+    if(instanceSize > 0){
+      instanceSize--;
+    }
+    return save;
   };
 
   someInstance.size = function(){
+    return instanceSize;
   };
 
   return someInstance;
 };
+
