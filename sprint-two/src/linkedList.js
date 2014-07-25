df4f927e3f3f5bac0311cbf5a16d68cd93f04cef
dscx <dnchmbrln@gmail.com>
2014-07-25 10:36:42 -0700

    add new tail passes

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,35 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    if(!this.tail){
+      this.tail = {};
+      this.tail.value = value;
+    }
+    else if (this.tail){
+      var previous = this.tail;
+      previous.next = value; 
+      this.tail.value = value;
+      console.log(this.tail.value);
+    }
+    //check if tail exists
+    //(re)define tail
+    //"push" in value
   };
 
   list.removeHead = function(){
+    //save current head?
+    //check if current head exists
+    //define new head
+
   };
 
   list.contains = function(target){
+    //scan through each "node'' or linked "array"
+    //if target exists do work otherwise return something
+    //return boolean
+
+
+
   };
 
   return list;
