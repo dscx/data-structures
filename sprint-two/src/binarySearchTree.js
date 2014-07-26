81037a72e4f566264d983e38fe5a9f79d661d21f
dscx <dnchmbrln@gmail.com>
2014-07-25 22:15:57 -0700

    solved first prompt

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,25 @@
 var makeBinarySearchTree = function(value){
+
+  var binaryTree = Object.create(binaryMethods);
+  binaryTree.left = null;
+  binaryTree.right = null;
+  binaryTree.value = value;
+
+  return binaryTree;
 };
 
+var binaryMethods ={
+  insert : function(){
 
+  },
+  contains: function(){
+
+  },
+  depthFirstLog: function(){
+
+  }
+
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
