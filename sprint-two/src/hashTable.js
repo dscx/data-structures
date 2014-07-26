b60dfaf180deb5313b5d722d31772911c3c8cf47
dscx <dnchmbrln@gmail.com>
2014-07-25 22:01:42 -0700

    completed hashtable solutions and some refactoring

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -17,24 +17,33 @@ HashTable.prototype.insert = function(k, v){
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
   var results = this._storage.get(i);
-  if(results.length  === 1)  {
-    return results[0][1];
-  }
-  else if(results.length > 1){
     for(var j = 0; j < results.length; j++){
       if(results[j][0] === k){
         return results[j][1];
       }
-    }
-  }
+    } return null;
 };
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(i, null);
+  var results = this._storage.get(i);
+
+  for(var j = 0; j < results.length; j++){
+    if(results[j][0] === k){
+      results.splice(j,1);
+      this._storage.set(i,results);
+      return;
+    }
+}
 };
 
+// results = [   [v1,v1], [v2,v2], [v3,null], [v4,v4]   ]
 
+// results[2] = null;
+
+// results = [   [v1,v1], [v2,v2], [v4,v4]  ]
+
+// set(i, results)
 
 /*
  * Complexity: What is the time complexity of the above functions?
