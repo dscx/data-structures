7aab994811d000d536656d905aa49b89e4ee320a
dscx <dnchmbrln@gmail.com>
2014-07-25 22:53:36 -0700

    solved second prompt

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -9,8 +9,30 @@ var makeBinarySearchTree = function(value){
 };
 
 var binaryMethods ={
-  insert : function(){
+  insert : function(value){  
+    //compare value to thisvalue
+  var traverse = function(node, direction){
+    if(!node[direction]){
+      node[direction] = makeBinarySearchTree(value);
+    }
+    else{
+      if(value < node[direction].value){
+        traverse(node[direction], "left");
+      }
+      else{
+        traverse(node[direction], "right");
+      }
+    }
+  };
 
+ var chooseDirection = value < this.value ? "left" : "right";
+    
+  traverse(this, chooseDirection);
+  
+    //determine which to direction to go
+      //continue until no "children" in planned direction
+      //create a new object
+        //assign property
   },
   contains: function(){
 
