75ab01197dd451f1b45e71b52bcfb11ab4e0fdbe
dscx <dnchmbrln@gmail.com>
2014-07-25 20:26:34 -0700

    solved all but last prompt

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,23 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  console.log(this);
+  this._storage.set(i, v);
+  
+  //set values into storage array?
+  //
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(i);
+
 
 };
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(i, null);
 };
 
 
