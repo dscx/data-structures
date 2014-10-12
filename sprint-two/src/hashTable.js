8955607205e6bdb624b5819a933217854c16e369
dscx <dnchmbrln@gmail.com>
2014-10-12 11:56:32 -0700

    fix

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,17 +1,19 @@
 var HashTable = function(){
   this._limit = 8;
   this._storage = makeLimitedArray(this._limit);
+
+
 };
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  var existing = this._storage.get(i);
-  if(existing){
-    existing.push([k,v]);
-    this._storage.set(i, existing);
-  } else {
-    this._storage.set(i, [[k,v]]);
+  var bucket = this._storage.get(i);
+
+  if(!bucket){
+    bucket = [];
+    this._storage.set(i, bucket);
   }
+    bucket.push([k,v]);
 };
 
 HashTable.prototype.retrieve = function(k){
@@ -37,13 +39,8 @@ HashTable.prototype.remove = function(k){
 }
 };
 
-// results = [   [v1,v1], [v2,v2], [v3,null], [v4,v4]   ]
-
-// results[2] = null;
-
-// results = [   [v1,v1], [v2,v2], [v4,v4]  ]
-
-// set(i, results)
+  //need to track size
+  //need hasthtable prototype for resize
 
 /*
  * Complexity: What is the time complexity of the above functions?
